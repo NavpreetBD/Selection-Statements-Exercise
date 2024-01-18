9ec5686121ed864d768da7afa7b66e205c1df85b
navpreet <navpreetbatth2.nb@gmail.com>
2024-01-18 11:35:52 -0800

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            // Initialize favorite number
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            // Prompt user to guess the number
+            Console.WriteLine("Guess my favorite number (between 1 and 1000):");
+            var userInput = int.Parse(Console.ReadLine());
+
+            // Check if the guessed number is below, equal to, or above the initial value
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed it!!!");
+            }
     }
 }
